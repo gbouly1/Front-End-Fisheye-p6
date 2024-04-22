f06e01ca82edaca28f0b454c1cdb9077e73cb5ac
Guillaume Bouly <gbouly1@gmail.com>
2024-04-22 14:50:48 +0200

    récupération des datas + style de la landing page

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,28 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const cityElement = document.createElement("p");
+    cityElement.innerHTML = `${city}, ${country}`;
+    cityElement.classList.add("cityElement");
+    const taglineElement = document.createElement("p");
+    taglineElement.innerHTML = tagline;
+    const priceElement = document.createElement("p");
+    priceElement.innerHTML = `${price}€/jours`;
+    priceElement.classList.add("priceElement");
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(cityElement);
+    article.appendChild(taglineElement);
+    article.appendChild(priceElement);
+    return article;
+  }
+  return { name, picture, getUserCardDOM, city, country, tagline, price };
 }
