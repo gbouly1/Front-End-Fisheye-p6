34d7474a90738ceaf674a95cb646c50c5511a765
Guillaume Bouly <gbouly1@gmail.com>
2024-09-16 12:45:24 +0200

    Gérer les modales avec les touches du clavier

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,24 +2,24 @@ import { firstName } from "../pages/index.js";
 
 const modalBtn = document.querySelector(".contact_button");
 const closeBtn = document.querySelector(".close_button");
-// on recupère le main et le header pour l'opacité lorsque la modal est open
 const headerPage = document.getElementById("header");
 const mainPage = document.getElementById("main");
-// on recupère les input pour les afficher dans la console
 const myForm = document.getElementById("form");
 const prenom = document.getElementById("prenom");
 const nom = document.getElementById("nom");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 
+const modal = document.getElementById("contact_modal");
+
 // on affiche la modal et applique le style au header et au main
 function displayModal() {
-  const modal = document.getElementById("contact_modal");
   const nameElement = document.querySelector(".first-name");
   nameElement.textContent = firstName;
   headerPage.classList.add("opacity-fifty");
   mainPage.classList.add("opacity-fifty");
   modal.style.display = "block";
+  document.addEventListener("keydown", handleKeyDown); // Ajouter l'écouteur d'événements du clavier
 }
 
 modalBtn.addEventListener("click", (e) => {
@@ -29,16 +29,23 @@ modalBtn.addEventListener("click", (e) => {
 
 // on ferme la modal et enlève le style du header et main
 function closeModal() {
-  const modal = document.getElementById("contact_modal");
   headerPage.classList.remove("opacity-fifty");
   mainPage.classList.remove("opacity-fifty");
   modal.style.display = "none";
+  document.removeEventListener("keydown", handleKeyDown); // Supprimer l'écouteur d'événements du clavier
 }
 
 closeBtn.addEventListener("click", (e) => {
   e.preventDefault();
   closeModal();
 });
+
+// Fermeture avec la touche Échappe
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+}
 
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -5,18 +5,15 @@ export function addImageArrayForNextPrevious(image) {
   imagesArray.push(image);
 }
 
-// let currentIndex = 0;
 const main = document.getElementById("main");
 const lightboxImg = document.querySelector(".lightbox-img");
 const lightboxTitle = document.querySelector(".lightbox-title");
 const lightbox = document.querySelector(".lightbox");
-// Sélectionnez tous les éléments d'images de votre template
 
 // Sélectionnez les éléments de la lightbox
 const closeLightbox = document.querySelector(".lightbox-close");
 const lightboxPrevBtn = document.querySelector(".lightbox-prev");
 const lightboxNextBtn = document.querySelector(".lightbox-next");
-// const lightboxCloseBtn = document.querySelector(".lightbox-close");
 
 // Vérifie si on est sur la bonne page et que l'id est présent
 if (
@@ -24,32 +21,66 @@ if (
   window.location.search.match(/id=\d+/)
 ) {
   // Gestionnaire d'événements pour le bouton "Précédent"
-  lightboxPrevBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + imagesArray.length) % imagesArray.length;
-    updateLightboxContent();
-  });
+  lightboxPrevBtn.addEventListener("click", showPreviousMedia);
 
   // Gestionnaire d'événements pour le bouton "Suivant"
-  lightboxNextBtn.addEventListener("click", () => {
-    currentIndex = (currentIndex + 1) % imagesArray.length;
-    updateLightboxContent();
-  });
+  lightboxNextBtn.addEventListener("click", showNextMedia);
 
   closeLightbox.addEventListener("click", (event) => {
     event.preventDefault();
-    lightbox.style.display = "none";
-    main.style.display = "block";
+    closeLightboxModal();
   });
+
+  document.addEventListener("keydown", handleKeyDown);
+}
+
+// Fonction pour afficher le média précédent
+function showPreviousMedia() {
+  currentIndex = (currentIndex - 1 + imagesArray.length) % imagesArray.length;
+  updateLightboxContent();
+}
+
+// Fonction pour afficher le média suivant
+function showNextMedia() {
+  currentIndex = (currentIndex + 1) % imagesArray.length;
+  updateLightboxContent();
+}
+
+// Fonction pour fermer la lightbox
+function closeLightboxModal() {
+  lightbox.style.display = "none";
+  main.style.display = "block";
+}
+
+// Gestionnaire pour les touches du clavier
+function handleKeyDown(event) {
+  if (lightbox.style.display === "flex") {
+    switch (event.key) {
+      case "ArrowLeft":
+        showPreviousMedia();
+        break;
+      case "ArrowRight":
+        showNextMedia();
+        break;
+      case "Escape":
+        closeLightboxModal();
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 // Ouverture de la lightbox
 export function openLightbox(mediaData, type = "image") {
   const media = imagesArray.find((m) => m.url === mediaData);
   currentIndex = imagesArray.indexOf(media);
+
   // Supprimer le contenu précédent de la lightbox
   while (lightboxImg.firstChild) {
     lightboxImg.removeChild(lightboxImg.firstChild);
   }
+
   if (type === "image") {
     const img = document.createElement("img");
     img.src = mediaData;
@@ -70,7 +101,7 @@ export function openLightbox(mediaData, type = "image") {
   lightbox.style.display = "flex";
 }
 
-// Update de la lightbox par rapport a son index
+// Update de la lightbox par rapport à l'index
 function updateLightboxContent() {
   const media = imagesArray[currentIndex];
   const mediaData = media.url;
@@ -80,19 +111,21 @@ function updateLightboxContent() {
   while (lightboxImg.firstChild) {
     lightboxImg.removeChild(lightboxImg.firstChild);
   }
+
   if (type === "image") {
     const img = document.createElement("img");
     img.src = mediaData;
-    img.alt = mediaData;
+    img.alt = media.title;
     img.setAttribute("data-type", "image");
     lightboxImg.appendChild(img);
   } else if (type === "video") {
     const video = document.createElement("video");
     video.src = mediaData;
-    video.alt = mediaData;
+    video.alt = media.title;
     video.controls = true;
     video.setAttribute("data-type", "video");
     lightboxImg.appendChild(video);
   }
+
   lightboxTitle.textContent = media.title;
 }
